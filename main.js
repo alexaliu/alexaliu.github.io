e1cb32f45ea2d55aaa1eab1c01c52780deeb5490
Alex Liu <alex.allen.liu@gmail.com>
2014-12-26 23:27:22 -0800

    SCROLLBARS ARE HARD

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log($('.sidebar').width())
+
 var navArrowDeg = true
 $('.nav-no-fix').click(function(){
     $('.navbar').toggleClass('navbar-fixed-top navbar-static-top');
@@ -42,11 +44,46 @@ $('.sidebar-list>li').click(function(){
     $('.' + selected.toLowerCase()).removeClass('nodisplay');
 })
 
-// $( window ).resize(function(){
-//     var fontSize = $('.sidebar-name-div').width() * 0.1
-//     $('.sidebar-name').css('font-size', fontSize);
-//     console.log(fontSize);
-// });
+$( window ).resize(function(){
+    $('.scroll-pane').jScrollPane({showArrows: true, contentWidth: '500px'});
+});
+
+
+$(function(){
+    var win = $(window);
+    // Full body scroll
+    var isResizing = false;
+    win.bind(
+        'resize',
+        function(){
+            if (!isResizing) {
+                isResizing = true;
+                var container = $('#full-page-container');
+                // Temporarily make the container tiny so it doesn't influence the
+                // calculation of the size of the document
+                container.css({'width': 1, 'height': 1});
+                // Now make it the size of the window...
+                container.css({'width': win.width(), 'height': win.height()});
+                isResizing = false;
+                container.jScrollPane({'showArrows': true});
+            }
+        }
+    ).trigger('resize');
+
+    // Workaround for known Opera issue which breaks demo (see
+    // http://jscrollpane.kelvinluck.com/known_issues.html#opera-scrollbar )
+    $('body').css('overflow', 'hidden');
+
+    // IE calculates the width incorrectly first time round (it
+    // doesn't count the space used by the native scrollbar) so
+    // we re-trigger if necessary.
+    if ($('#full-page-container').width() != win.width()) {
+        win.trigger('resize');
+    }
+
+    // Internal scrollpanes
+    $('.scroll-pane').jScrollPane({showArrows: true, contentWidth: '500px'});
+});
 
 
 
