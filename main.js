36ddbdd36ed2191e5e312e8859af1ea881af4779
Alex Liu <alex.allen.liu@gmail.com>
2014-12-27 16:46:34 -0800

    changed to nano scroller

--- main.js
+++ main.js
@@ -38,51 +38,8 @@ $('.sidebar-list>li').click(function(){
         $(this).addClass('nodisplay');
     })
     $('.' + selected.toLowerCase()).removeClass('nodisplay');
-    pageScroll();
+    heightResize();
 });
-
-// Full body scroll example from JScrollPane
-var pageScroll = function(){
-    var win = $(window);
-    // Full body scroll
-    var isResizing = false;
-    win.bind(
-        'resize',
-        function(){
-            if (!isResizing) {
-                isResizing = true;
-                var container = $('#full-page-container');
-                // Temporarily make the container tiny so it doesn't influence the
-                // calculation of the size of the document
-                container.css({'width': 0, 'height': 0});
-                // Now make it the size of the window...
-                container.css({'width': win.width(), 'height': win.height()});
-                isResizing = false;
-                container.jScrollPane();
-            }
-        }
-    ).trigger('resize');
-
-    // Workaround for known Opera issue 
-    $('body').css('overflow', 'hidden');
-    // IE
-    if ($('#full-page-container').width() != win.width()) { win.trigger('resize'); }
-
-    // Internal scrollpanes
-    $('.scroll-pane').jScrollPane({contentWidth: '500px'});
-    sidebarFix();
-    $('.navbar').css('width', '');
-    // $('.navbar').css('width', '100%');
-};
-
-// Scrollbars make sidebars 20 pixels too short. This fixes that
-var sidebarFix = function(){
-    var sidebar = $('.sidebar-list-div')
-    var width = sidebar.width()
-    console.log(width);
-    sidebar.css('width', width + 20);
-    $(sidebar.children()[0]).css('width', '100%');
-}
 
 // I think the scrollbars make 100vh not work properly when the content is longer
 // than 100vh, so this will figure out whether the height should be 100% or 100vh,
@@ -100,9 +57,9 @@ var heightResize = function(){
 
 $( window ).resize(function(){
     $('.scroll-pane').jScrollPane({contentWidth: '500px'});
-    sidebarFix();
     heightResize();
 });
 
-$(pageScroll);
+$($(".nano").nanoScroller());
+
 $(heightResize);
